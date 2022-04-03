5b313e7cf8bb2b7f45e71780606054b00ac9c838
Austine D. Odhiambo <austine.declan@gmail.com>
2022-04-04 01:43:10 +0300

    Added a way to check game state and evaluate win condition

--- TicTacToe.py
+++ TicTacToe.py
@@ -126,8 +126,49 @@ def gameInput(buttonPress):
 def checkWinState(player, buttonId):
     global gameState
     gameState[buttonId - 1] = player
-    print(gameState)
+    # print(gameState)
 
+    # if(buttonId == 1 and button2['text'] == player and button3['text'] == player):
+
+    if(gameState[0] == 'X' and gameState[1] == 'X'  and gameState[2] == 'X' or gameState[0] == 'O' and gameState[1] == 'O'  and gameState[2] == 'O'):
+        print('Win codition met')
+
+    if(gameState[0] == 'X' and gameState[1] == 'X'  and gameState[2] == 'X' or gameState[0] == 'O' and gameState[1] == 'O'  and gameState[2] == 'O'):
+        print('Win codition met')
+
+    if(gameState[0] == 'X' and gameState[3] == 'X'  and gameState[6] == 'X' or gameState[0] == 'O' and gameState[3] == 'O'  and gameState[6] == 'O'):
+        print('Win codition met')     
+
+    if(gameState[0] == 'X' and gameState[4] == 'X'  and gameState[8] == 'X' or gameState[0] == 'O' and gameState[4] == 'O'  and gameState[8] == 'O'):
+        print('Win codition met')
+
+    if(gameState[1] == 'X' and gameState[4] == 'X'  and gameState[7] == 'X' or gameState[1] == 'O' and gameState[4] == 'O'  and gameState[7] == 'O'):
+        print('Win codition met')
+
+    if(gameState[2] == 'X' and gameState[4] == 'X'  and gameState[6] == 'X' or gameState[2] == 'O' and gameState[4] == 'O'  and gameState[6] == 'O'):
+        print('Win codition met')
+
+    if(gameState[2] == 'X' and gameState[5] == 'X'  and gameState[8] == 'X' or gameState[2] == 'O' and gameState[5] == 'O'  and gameState[8] == 'O'):
+        print('Win codition met')
+    
+    if(gameState[3] == 'X' and gameState[4] == 'X'  and gameState[5] == 'X' or gameState[3] == 'O' and gameState[4] == 'O'  and gameState[5] == 'O'):
+        print('Win codition met')
+    
+    if(gameState[6] == 'X' and gameState[7] == 'X'  and gameState[8] == 'X' or gameState[6] == 'O' and gameState[7] == 'O'  and gameState[8] == 'O'):
+        print('Win codition met')
+    
+
+    #if(buttonId == 1):
+    #    if(player == 'X'):
+    #       if(button2['text'] == 'X' and button3['text'] == 'X'):
+    #            print('X wins')
+    #        else:
+    #            print('win condition not met')
+    #    elif(player == 'O'):
+    #        if(button2['text'] == 'O' and button3['text'] == 'O'):
+    #            print('O wins')
+    #        else:
+    #            print('win condition not met')
 
 
 # create window

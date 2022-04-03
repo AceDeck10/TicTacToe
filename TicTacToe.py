f6d6506c609fc9a14c7d056e2fa7590d80df4600
Austine D. Odhiambo <austine.declan@gmail.com>
2022-04-04 02:52:52 +0300

    Added pop up message after win

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 isOTurn = False
 gameState = [None] * 9
@@ -12,7 +13,7 @@ def gameInput(buttonPress):
     # Check for button presses
     if(buttonPress == 1):
         global isOTurn
-        # check which player's turn it is (repeated fo all buttons)
+        # check which player's turn it is (repeated for all buttons)
         if(not(isOTurn)):
             button1.config(text = x)
             button1.config(state = DISABLED)
@@ -126,50 +127,101 @@ def gameInput(buttonPress):
 def checkWinState(player, buttonId):
     global gameState
     gameState[buttonId - 1] = player
-    # print(gameState)
-
-    # if(buttonId == 1 and button2['text'] == player and button3['text'] == player):
 
     if(gameState[0] == 'X' and gameState[1] == 'X'  and gameState[2] == 'X' or gameState[0] == 'O' and gameState[1] == 'O'  and gameState[2] == 'O'):
-        print('Win codition met')
-
-    if(gameState[0] == 'X' and gameState[1] == 'X'  and gameState[2] == 'X' or gameState[0] == 'O' and gameState[1] == 'O'  and gameState[2] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
     if(gameState[0] == 'X' and gameState[3] == 'X'  and gameState[6] == 'X' or gameState[0] == 'O' and gameState[3] == 'O'  and gameState[6] == 'O'):
-        print('Win codition met')     
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
     if(gameState[0] == 'X' and gameState[4] == 'X'  and gameState[8] == 'X' or gameState[0] == 'O' and gameState[4] == 'O'  and gameState[8] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
     if(gameState[1] == 'X' and gameState[4] == 'X'  and gameState[7] == 'X' or gameState[1] == 'O' and gameState[4] == 'O'  and gameState[7] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
     if(gameState[2] == 'X' and gameState[4] == 'X'  and gameState[6] == 'X' or gameState[2] == 'O' and gameState[4] == 'O'  and gameState[6] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
     if(gameState[2] == 'X' and gameState[5] == 'X'  and gameState[8] == 'X' or gameState[2] == 'O' and gameState[5] == 'O'  and gameState[8] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
     
     if(gameState[3] == 'X' and gameState[4] == 'X'  and gameState[5] == 'X' or gameState[3] == 'O' and gameState[4] == 'O'  and gameState[5] == 'O'):
-        print('Win codition met')
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
     
     if(gameState[6] == 'X' and gameState[7] == 'X'  and gameState[8] == 'X' or gameState[6] == 'O' and gameState[7] == 'O'  and gameState[8] == 'O'):
-        print('Win codition met')
-    
+        if(player == 'X'):
+            messagebox.showinfo('Message', 'X Wins!')
+            resetGame()
+        elif(player == 'O'):
+            messagebox.showinfo('Message', 'O Wins!')
+            resetGame()
 
-    #if(buttonId == 1):
-    #    if(player == 'X'):
-    #       if(button2['text'] == 'X' and button3['text'] == 'X'):
-    #            print('X wins')
-    #        else:
-    #            print('win condition not met')
-    #    elif(player == 'O'):
-    #        if(button2['text'] == 'O' and button3['text'] == 'O'):
-    #            print('O wins')
-    #        else:
-    #            print('win condition not met')
+# Reset game after player win or game over
+# Game logic breaks down after this function is called
+# TODO fix later
+def resetGame():
+    button1.config(text = '')
+    button1.config(state = NORMAL)
 
+    button2.config(text = '')
+    button2.config(state = NORMAL)
+
+    button3.config(text = '')
+    button3.config(state = NORMAL)
+
+    button4.config(text = '')
+    button4.config(state = NORMAL)
+
+    button5.config(text = '')
+    button5.config(state = NORMAL)
+
+    button6.config(text = '')
+    button6.config(state = NORMAL)
+
+    button7.config(text = '')
+    button7.config(state = NORMAL)
+
+    button8.config(text = '')
+    button8.config(state = NORMAL)
+
+    button9.config(text = '')
+    button9.config(state = NORMAL)
 
 # create window
 window = Tk()
@@ -177,54 +229,54 @@ button1 = Button(window, text='')
 button1.config(command = lambda m = 1: gameInput(m))
 button1.config(font = ('Ink Free', 50, 'bold'))
 button1.config(height = 1, width = 2)
-button1.grid(row=1, column=1)
+button1.grid(row = 1, column = 1)
 
 button2 = Button(window, text='')
 button2.config(command = lambda m = 2: gameInput(m))
 button2.config(font = ('Ink Free', 50, 'bold'))
 button2.config(height = 1, width = 2)
-button2.grid(row=1, column=2)
+button2.grid(row = 1, column = 2)
 
 button3 = Button(window, text='')
 button3.config(command = lambda m = 3: gameInput(m))
 button3.config(font = ('Ink Free', 50, 'bold'))
 button3.config(height = 1, width = 2)
-button3.grid(row=1, column=3)
+button3.grid(row = 1, column = 3)
 
 button4 = Button(window, text='')
 button4.config(command = lambda m = 4: gameInput(m))
 button4.config(font = ('Ink Free', 50, 'bold'))
 button4.config(height = 1, width = 2)
-button4.grid(row=2, column=1)
+button4.grid(row = 2, column = 1)
 
 button5 = Button(window, text='')
 button5.config(command = lambda m = 5: gameInput(m))
 button5.config(font = ('Ink Free', 50, 'bold'))
 button5.config(height = 1, width = 2)
-button5.grid(row=2, column=2)
+button5.grid(row = 2, column = 2)
 
 button6 = Button(window, text='')
 button6.config(command = lambda m = 6: gameInput(m))
 button6.config(font = ('Ink Free', 50, 'bold'))
 button6.config(height = 1, width = 2)
-button6.grid(row=2, column=3)
+button6.grid(row = 2, column = 3)
 
 button7 = Button(window, text='')
 button7.config(command = lambda m = 7: gameInput(m))
 button7.config(font = ('Ink Free', 50, 'bold'))
 button7.config(height = 1, width = 2)
-button7.grid(row=3, column=1)
+button7.grid(row = 3, column = 1)
 
 button8 = Button(window, text='')
 button8.config(command = lambda m = 8: gameInput(m))
 button8.config(font = ('Ink Free', 50, 'bold'))
 button8.config(height = 1, width = 2)
-button8.grid(row=3, column=2)
+button8.grid(row = 3, column = 2)
 
 button9 = Button(window, text='')
 button9.config(command = lambda m = 9: gameInput(m))
 button9.config(font = ('Ink Free', 50, 'bold'))
 button9.config(height = 1, width = 2)
-button9.grid(row=3, column=3)
+button9.grid(row = 3, column = 3)
 
 window.mainloop() 

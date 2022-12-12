f87184c08dd06d42e6881b65c37f921daf10cf0c
Austine D. Odhiambo <austine.declan@gmail.com>
2022-12-13 02:07:16 +0300

    Added a splash screen. Changed the program to generate the buttons dynamically and store them in a list

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,6 +10,7 @@
 #******************************************************************************
 
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 import tkinter.font as tkFont
 
@@ -17,6 +18,15 @@ import tkinter.font as tkFont
 isOTurn = False
 # Game state variable (List): Used to store game state
 gameState = [None] * 9
+
+#Splash screen
+splashScreen = Tk()
+splashScreen.title("Splash")
+splashScreen.geometry("300x200")
+splashScreen.overrideredirect(True)
+splashLabel = Label(splashScreen, text="Tic Tac Toe\n An Ace Declan game", font=('Montserat', 12))
+splashLabel.pack(pady=20)
+splashScreen.eval('tk::PlaceWindow . center')
 
 # Game I/O function
 # Gets the buttons presses, checks the player who pressed, displays letter on the button
@@ -29,109 +39,109 @@ def gameInput(buttonPress):
     if(buttonPress == 1):
         # check which player's turn it is (repeated for all buttons)
         if(not(isOTurn)):
-            button1.config(text = x)
-            button1.config(state = DISABLED)
+            buttons[0].config(text = x)
+            buttons[0].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button1.config(text = o)
-            button1.config(state = DISABLED)
+            buttons[0].config(text = o)
+            buttons[0].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 2):
         if(not(isOTurn)):
-            button2.config(text = x)
-            button2.config(state = DISABLED)
+            buttons[1].config(text = x)
+            buttons[1].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button2.config(text = o)
-            button2.config(state = DISABLED)
+            buttons[1].config(text = o)
+            buttons[1].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 3):
         if(not(isOTurn)):
-            button3.config(text = x)
-            button3.config(state = DISABLED)
+            buttons[2].config(text = x)
+            buttons[2].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button3.config(text = o)
-            button3.config(state = DISABLED)
+            buttons[2].config(text = o)
+            buttons[2].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 4):
         if(not(isOTurn)):
-            button4.config(text = x)
-            button4.config(state = DISABLED)
+            buttons[3].config(text = x)
+            buttons[3].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button4.config(text = o)
-            button4.config(state = DISABLED)
+            buttons[3].config(text = o)
+            buttons[3].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 5):
         if(not(isOTurn)):
-            button5.config(text = x)
-            button5.config(state = DISABLED)
+            buttons[4].config(text = x)
+            buttons[4].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button5.config(text = o)
-            button5.config(state = DISABLED)
+            buttons[4].config(text = o)
+            buttons[4].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 6):
         if(not(isOTurn)):
-            button6.config(text = x)
-            button6.config(state = DISABLED)
+            buttons[5].config(text = x)
+            buttons[5].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button6.config(text = o)
-            button6.config(state = DISABLED)
+            button[5].config(text = o)
+            buttons[5].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 7):
         if(not(isOTurn)):
-            button7.config(text = x)
-            button7.config(state = DISABLED)
+            buttons[6].config(text = x)
+            buttons[6].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button7.config(text = o)
-            button7.config(state = DISABLED)
+            buttons[6].config(text = o)
+            buttons[6].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 8):
         if(not(isOTurn)):
-            button8.config(text = x)
-            button8.config(state = DISABLED)
+            buttons[7].config(text = x)
+            buttons[7].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button8.config(text = o)
-            button8.config(state = DISABLED)
+            buttons[7].config(text = o)
+            buttons[7].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
     elif(buttonPress == 9):
         if(not(isOTurn)):
-            button9.config(text = x)
-            button9.config(state = DISABLED)
+            buttons[8].config(text = x)
+            buttons[8].config(state = DISABLED)
             isOTurn = True
             checkWinState(x, buttonPress)
         elif(isOTurn):
-            button9.config(text = o)
-            button9.config(state = DISABLED)
+            buttons[8].config(text = o)
+            buttons[8].config(state = DISABLED)
             isOTurn = False
             checkWinState(o, buttonPress)
 
@@ -238,32 +248,32 @@ def resetGame():
     isOTurn = False
     gameState = [None] * 9
 
-    button1.config(text = '')
-    button1.config(state = NORMAL)
+    buttons[0].config(text = '')
+    buttons[0].config(state = NORMAL)
 
-    button2.config(text = '')
-    button2.config(state = NORMAL)
+    buttons[1].config(text = '')
+    buttons[1].config(state = NORMAL)
 
-    button3.config(text = '')
-    button3.config(state = NORMAL)
+    buttons[2].config(text = '')
+    buttons[2].config(state = NORMAL)
 
-    button4.config(text = '')
-    button4.config(state = NORMAL)
+    buttons[3].config(text = '')
+    buttons[3].config(state = NORMAL)
 
-    button5.config(text = '')
-    button5.config(state = NORMAL)
+    buttons[4].config(text = '')
+    buttons[4].config(state = NORMAL)
 
-    button6.config(text = '')
-    button6.config(state = NORMAL)
+    buttons[5].config(text = '')
+    buttons[5].config(state = NORMAL)
 
-    button7.config(text = '')
-    button7.config(state = NORMAL)
+    buttons[6].config(text = '')
+    buttons[6].config(state = NORMAL)
 
-    button8.config(text = '')
-    button8.config(state = NORMAL)
+    buttons[7].config(text = '')
+    buttons[7].config(state = NORMAL)
 
-    button9.config(text = '')
-    button9.config(state = NORMAL)
+    buttons[8].config(text = '')
+    buttons[8].config(state = NORMAL)
 
 # Right click menu function
 def showRightClickMenu(event):
@@ -272,86 +282,61 @@ def showRightClickMenu(event):
     finally:
         rightClickMenu.grab_release()
 
-# Create main gui
-window = Tk()
-window.title("Tic Tac Toe")
-window.geometry('435x435')
-window.resizable(False, False)
+#Main window
+def mainWindow():
+    # Destroy spash screen
+    try:
+        splashScreen
+    except NameError:
+        splashExists = False
+    else:
+        splashExists = True
+
+    if(splashExists == True):
+        splashScreen.destroy()
 
 
+    window = Tk()
+    window.title("Tic Tac Toe")
+    window.geometry('435x435')
+    window.resizable(False, False)
 
-# Size argument of the tkFont.Font() function is what sets the size of the button
-font = tkFont.Font(family="Helvetica", size=90, weight = tkFont.BOLD)
-blankImage = PhotoImage()
+    # TODO: use for loop to generate buttons
+    global buttons
+    global button
+    buttons = list()
 
-# TODO: use for loop to generate buttons
-button1 = Button(window, image = blankImage, text='', compound = CENTER)
-button1.config(command = lambda m = 1: gameInput(m))
-button1.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button1.config(height = square_size, width = square_size)
-button1.grid(row = 1, column = 1)
+    for i in range(3):
+        for j in range(3):
+            if(i == 0):
+                k = i + j
+            elif(i == 1):
+                k = i + j + 2
+            elif(i == 2):
+                k = i + j + 4
+            else:
+                print('error while creating buttons')
 
-button2 = Button(window, image = blankImage, text='', compound = CENTER)
-button2.config(command = lambda m = 2: gameInput(m))
-button2.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button2.config(height = square_size, width = square_size)
-button2.grid(row = 1, column = 2)
+            buttonFrame = Frame(width=145,height=145)
 
-button3 = Button(window, image = blankImage, text='', compound = CENTER)
-button3.config(command = lambda m = 3: gameInput(m))
-button3.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button3.config(height = square_size, width = square_size)
-button3.grid(row = 1, column = 3)
+            buttons.append(Button(buttonFrame, text='', compound = CENTER))
+               
+            buttons[k].config(command = lambda m = k + 1: gameInput(m))
+            buttons[k].config(font = ('Ink Free', 75, 'bold'))
 
-button4 = Button(window, image = blankImage, text='', compound = CENTER)
-button4.config(command = lambda m = 4: gameInput(m))
-button4.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button4.config(height = square_size, width = square_size)
-button4.grid(row = 2, column = 1)
+            buttonFrame.rowconfigure(0, weight = 1)
+            buttonFrame.columnconfigure(0, weight = 1)
+            buttonFrame.grid_propagate(0)
+            buttonFrame.grid(row = j + 1, column = i + 1)
 
-button5 = Button(window, image = blankImage, text='', compound = CENTER)
-button5.config(command = lambda m = 5: gameInput(m))
-button5.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button5.config(height = square_size, width = square_size)
-button5.grid(row = 2, column = 2)
+            buttons[k].grid(sticky = "NWSE")
 
-button6 = Button(window, image = blankImage, text='', compound = CENTER)
-button6.config(command = lambda m = 6: gameInput(m))
-button6.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button6.config(height = square_size, width = square_size)
-button6.grid(row = 2, column = 3)
+    global rightClickMenu
+    rightClickMenu = Menu(window, tearoff = 0)
+    rightClickMenu.add_command(label ="Restart", command = resetGame)
 
-button7 = Button(window, image = blankImage, text='', compound = CENTER)
-button7.config(command = lambda m = 7: gameInput(m))
-button7.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button7.config(height = square_size, width = square_size)
-button7.grid(row = 3, column = 1)
+    window.bind("<Button-3>", showRightClickMenu)
+    window.eval('tk::PlaceWindow . center')
 
-button8 = Button(window, image = blankImage, text='', compound = CENTER)
-button8.config(command = lambda m = 8: gameInput(m))
-button8.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button8.config(height = square_size, width = square_size)
-button8.grid(row = 3, column = 2)
-
-button9 = Button(window, image = blankImage, text='', compound = CENTER)
-button9.config(command = lambda m = 9: gameInput(m))
-button9.config(font = ('Ink Free', 75, 'bold'))
-square_size = font.metrics('linespace')
-button9.config(height = square_size, width = square_size)
-button9.grid(row = 3, column = 3)
-
-rightClickMenu = Menu(window, tearoff = 0)
-rightClickMenu.add_command(label ="Restart", command = resetGame)
-
-window.bind("<Button-3>", showRightClickMenu)
-window.eval('tk::PlaceWindow . center')
-
-window.mainloop()
+splashScreen.after(2000, mainWindow)
+mainloop()
